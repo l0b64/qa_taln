30df4b80d6aa419273bfddc1cd243d7c8519cf83
lounis_ob <lounis.ob@gmail.com>
2023-02-27 08:07:10 +0100

    add requirements

--- README.md
+++ README.md
@@ -4,25 +4,30 @@
  
  # Documentation
  Le programme commence d'abord par extraire les questions en Anglais dans le fichier questions.xml afin de les traiter. La première étape du traitement consiste à essayer de trouver quel est le type de réponse à la question (personne, organisation, lieu ...) à l'aide du pronom en début et du contexte. Dans un second temps, on récupère l'entité nommée de la question à l'aide de la librairie Spacy et la relation en comparant les tokens de la phrase avec les relations fournis dans le fichier relations.xml afin de requêter la base de données DBpedia. Quand on dispose de ces deux informations qui sont une condition nécessaire, on passe à l'étape de la préparation et d'envoi de la requête vers DBpedia pour récupérer les réponses possibles aux questions traitées. Enfin, on analyse les résultats avec différentes métriques pour évaluer le système mis en place.
- 
- - Remarques et évolution possibles :
+
+* Remarques et évolution possibles :
 
 En souhaitant améliorer les résultats des requêtes ou affiner la recherche d'entité nommée, on peut utiliser des regex sur nos questions en entrée. Le souci serait de spécifier sur un jeu de données précis en entrée certes en améliorant les résultats pour celui-ci mais en perdant la robustesse et la capacité de généralisation du système on changeant les questions. J'ai donc fait le choix de ne pas utiliser de regex pour cette raison.
 
 L'algorithme de recherche de relations peut être amélioré, car certaines relations ne sont pas comparées avec les bon tokens ou la relation diffère des tokens de la phrase ce qui amène à des erreurs. D'autres approches algorithmiques différent de la distance de Levenshtein peuvent permettre de résoudre ce problème. 
  
   # Utilisation
- - Librairies nécessaires :
+ - Installation :
 
-math  
-re  
+Exécuter cette commande :
+```
+python -m spacy download en_core_web_sm
+```
+Librairies :
+```
 nltk  
 spacy  
 SPARQLWrapper   
 lxml  
-
+```
  - Fichiers nécessaires :
 
+```
 relations.xml -> contiens les différentes relations possibles  
 questions.xml -> contiens les questions et les réponses (URI DBpedia) attendues.  
- 
+ ```
--- taln.py
+++ taln.py
@@ -5,13 +5,9 @@ questions réponses en langue naturelle sur des données structurées
 
 import math
 import re
-
-import nltk
 import spacy
 from lxml import etree
 from nltk.metrics import *
-from spacy.lang.fr import French
-from spacy.tokenizer import Tokenizer
 from SPARQLWrapper import JSON, SPARQLWrapper
 
 LANGUAGE = "en"
@@ -20,118 +16,125 @@ nlp = spacy.load("en_core_web_sm")
 tokenizer = nlp.tokenizer
 
 questions = []
-réponsesAttendues = []
-réponses = []
+reponsesAttendues = []
+reponses = []
 
-#Parsing XML du fichier questions.xml  
+# Parsing XML du fichier questions.xml
 tree = etree.parse("./questions.xml")
 root = tree.getroot()
 
-#Récupérer les questions
-for q in root.findall('question/string'):
-     question = q.text
-     lang = q.get('lang')
+# Récupérer les questions
+for q in root.findall("question/string"):
+    question = q.text
+    lang = q.get("lang")
 
-     if(lang == LANGUAGE):
-       questions.append(str(question))
+    if lang == LANGUAGE:
+        questions.append(str(question))
 
-#Récupérer les réponses URI
-for a in root.findall('question/answers/answer/uri'):
-     answer = a.text
-     réponses.append(a.text)
+# Récupérer les réponses URI
+for a in root.findall("question/answers/answer/uri"):
+    answer = a.text
+    reponses.append(a.text)
 
-#REGEX sur le token du pronom intérrogatif
-def réponseAttendue(token):
-  if re.match(r"[Ww]ho",token):
-      réponsesAttendues.append("PERS")
-  elif re.match(r"[Ww]here",token):
-      réponsesAttendues.append("GEO")
-  elif re.match(r"[Ww]hen",token):
-      réponsesAttendues.append("TIM")
-  else:
-      réponsesAttendues.append('')
 
-#Trouver l'entité nommé pour une question donnée
+# REGEX sur le token du pronom intérrogatif
+def reponseAttendue(token):
+    if re.match(r"[Ww]ho", token):
+        reponsesAttendues.append("PERS")
+    elif re.match(r"[Ww]here", token):
+        reponsesAttendues.append("GEO")
+    elif re.match(r"[Ww]hen", token):
+        reponsesAttendues.append("TIM")
+    else:
+        reponsesAttendues.append("")
+
+
+# Trouver l'entité nommé pour une question donnée
 def NER(question):
-  ner = ""
-  doc = nlp(question)
-  for ent in doc.ents:
-      ner = (ent.text)# On récupère l'entité nommé par Spacy 
-  if(len(ner)== 0):#Si Spacy ne trouve l'entité nommé
-      ner = "NOT_FOUND"
-  
-  #Traitement sur l'entité nommé pour préparer la requête
-  ner = str(ner).replace("the ", "")
-  ner = str(ner).replace(" ", "_")
-  return ner
-  
-#Retourner la relation avec la distance de Levenshtein la plus proche des mots de la phrases
-def relationsAvec(question):
-  tokens = []
-  doc = nlp(question)
-  for token in doc:
-    tokens.append(token.text)
-  min_score = math.inf
-  relation = None
-  with open("relations.txt") as f:
-    for line in f:
-        line = line.replace("\n", "")
-        line_compare = re.sub(r'([a-z].*):',"",line)
-        for token in tokens:
-          score = edit_distance(token,line_compare)
-          if score < min_score:
-            min_score = score
-            relation = line
-  return relation
+    ner = ""
+    doc = nlp(question)
+    for ent in doc.ents:
+        ner = ent.text  # On récupère l'entité nommé par Spacy
+    if len(ner) == 0:  # Si Spacy ne trouve l'entité nommé
+        ner = "NOT_FOUND"
 
-#Préparation et envoie d'une requête vers DBpedia
-def requête(relation,entité):
-  sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-  query = "PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX res: <http://dbpedia.org/resource/> SELECT DISTINCT ?uri WHERE { res:"
-  query += entité + " " + relation + " ?uri . }"
-  sparql.setQuery(query)
-  sparql.setReturnFormat(JSON)
-  result = sparql.query().convert()
-  return result
+    # Traitement sur l'entité nommé pour préparer la requête
+    ner = str(ner).replace("the ", "")
+    ner = str(ner).replace(" ", "_")
+    return ner
+
+
+# Retourner la relation avec la distance de Levenshtein la plus proche des mots de la phrases
+def relationsAvec(question):
+    tokens = []
+    doc = nlp(question)
+    for token in doc:
+        tokens.append(token.text)
+    min_score = math.inf
+    relation = None
+    with open("relations.txt") as f:
+        for line in f:
+            line = line.replace("\n", "")
+            line_compare = re.sub(r"([a-z].*):", "", line)
+            for token in tokens:
+                score = edit_distance(token, line_compare)
+                if score < min_score:
+                    min_score = score
+                    relation = line
+    return relation
+
+
+# Préparation et envoie d'une requête vers DBpedia
+def requête(relation, entite):
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    query = "PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX res: <http://dbpedia.org/resource/> SELECT DISTINCT ?uri WHERE { res:"
+    query += entite + " " + relation + " ?uri . }"
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    result = sparql.query().convert()
+    return result
+
 
 #### Traitement des questions #####
 tab_result = []
 for question in questions:
+    doc = nlp(question)
+    for i, token in enumerate(doc):
+        if i == 0:  # PRONOM INTERROGATIF EN DEBUT DE PHRASE
+            reponseAttendue(token.text)
 
-  doc = nlp(question)
-  for i, token in enumerate(doc):
-    if(i==0):# PRONOM INTERROGATIF EN DEBUT DE PHRASE
-      réponseAttendue(token.text)
-      
-  if(NER(question) != "NOT_FOUND"):
-    results = requête(relationsAvec(question),NER(question))
-  for result in results["results"]["bindings"]:
-   tab_result.append(result["uri"]["value"])
-
-def recall(réponses,tab_result):
-  nb_correct = 0
-  for res in tab_result:
-    if res in réponses:
-      nb_correct += 1
-  return nb_correct/len(réponses)
-
-def précision(tab_result):
-  nb_correct = 0
-  for res in tab_result:
-    if res in réponses:
-      nb_correct += 1
-  return nb_correct/len(tab_result)
-
-def fmeasure(recall,précision):
-  return  (2*précision*recall)/(précision+recall)
+    if NER(question) != "NOT_FOUND":
+        results = requête(relationsAvec(question), NER(question))
+    for result in results["results"]["bindings"]:
+        tab_result.append(result["uri"]["value"])
 
 
-recall = recall(réponses,tab_result)
-précision = précision(tab_result)
-fmeasure = fmeasure(recall,précision)
+def recall(reponses, tab_result):
+    nb_correct = 0
+    for res in tab_result:
+        if res in reponses:
+            nb_correct += 1
+    return nb_correct / len(reponses)
+
+
+def precision(tab_result):
+    nb_correct = 0
+    for res in tab_result:
+        if res in reponses:
+            nb_correct += 1
+    return nb_correct / len(tab_result)
+
+
+def fmeasure(recall, precision):
+    return (2 * precision * recall) / (precision + recall)
+
+
+recall = recall(reponses, tab_result)
+precision_metric = precision(tab_result)
+fmeasure = fmeasure(recall, precision_metric)
 
 print("(---- Evaluation du système de questions et réponses en langage naturel ----)")
 print("      Rappel du sytème : " + str(recall))
-print("      Précision du système : " + str(précision))
+print("      Précision du système : " + str(precision_metric))
 print("      F-measure : " + str(fmeasure))
 print("(---------------------------------------------------------------------------)")
